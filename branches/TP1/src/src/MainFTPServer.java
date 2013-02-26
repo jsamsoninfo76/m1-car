02eaaace032f6054e04fdaec7fd8af189ec47902
victor.paumier@gmail.com <victor.paumier@gmail.com@fcdca30d-4911-e8bd-f8c9-4019da32051f>
2013-02-26 20:00:06 +0000

    thread

---
+++ branches/TP1/src/src/MainFTPServer.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Classe MainFTPServer correspondant au main qui initialise le serveur
+ * @author Jeremie Samson - Victor Paumier 
+ */
+
+public class MainFTPServer {
+    public static void main(String[] args) throws IOException {
+        int numeroSocket = 0;
+        int port = 7000;
+        int maxConnections = 0 ;
+        
+     // Création socket de control
+        
+        System.out.println("----- FTP-SERVER---- -----");
+        System.out.println("****************************");
+        System.out.println("Server Started...");
+        System.out.println("Listen on " + port);
+        System.out.println("Waiting for connections...");
+        System.out.println("-");
+        
+        try{
+            ServerSocket listener = new ServerSocket(port);
+            Socket server;
+
+            while((numeroSocket++ < maxConnections) || (maxConnections == 0)){
+
+              server = listener.accept();
+              ftpRequest conn_c= new ftpRequest(server, port, numeroSocket);
+              Thread t = new Thread(conn_c);
+              t.start();
+            }
+          } catch (IOException ioe) {
+            System.out.println("IOException on socket listen: " + ioe);
+            ioe.printStackTrace();
+          }
+    }
+}
